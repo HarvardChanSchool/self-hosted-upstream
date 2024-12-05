40e46625531a517204b85c3d6778ac62fbaea66c
Boone B Gorges <boonebgorges@gmail.com>
2024-12-05 10:48:56 -0600

    Update plugin: the-events-calendar.
    Name: The Events Calendar
    New version: 6.8.3
    Previous version: 6.8.1

--- wp-content/plugins/the-events-calendar/common/src/Tribe/Main.php
+++ wp-content/plugins/the-events-calendar/common/src/Tribe/Main.php
@@ -19,7 +19,7 @@ class Tribe__Main {
 	const OPTIONNAME        = 'tribe_events_calendar_options';
 	const OPTIONNAMENETWORK = 'tribe_events_calendar_network_options';
 	const FEED_URL          = 'https://theeventscalendar.com/feed/';
-	const VERSION           = '6.3.1';
+	const VERSION           = '6.4.0';
 
 	protected $plugin_context;
 	protected $plugin_context_class;
@@ -759,6 +759,7 @@ class Tribe__Main {
 		tribe_register_provider( Tribe\Service_Providers\Onboarding::class );
 		tribe_register_provider( Tribe\Admin\Notice\Service_Provider::class );
 		tribe_register_provider( \TEC\Common\Admin\Conditional_Content\Controller::class );
+		tribe_register_provider( \TEC\Common\Notifications\Controller::class );
 		tribe_register_provider( Libraries\Provider::class );
 
 		// Load the new third-party integration system.
@@ -766,6 +767,9 @@ class Tribe__Main {
 		// Load Site Health and Telemetry.
 		tribe_register_provider( TEC\Common\Site_Health\Provider::class );
 		tribe_register_provider( TEC\Common\Telemetry\Provider::class );
+
+		// Load Help Hub.
+		tribe_register_provider( TEC\Common\Admin\Help_Hub\Provider::class );
 	}
 
 	/**
--- wp-content/plugins/the-events-calendar/vendor/composer/installed.php
+++ wp-content/plugins/the-events-calendar/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'the-events-calendar/the-events-calendar',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '5058933c3df989dedb54919bd24288cf49e53336',
+        'reference' => '9ffb4ac71fe795219c504bee0664635ff023c2e8',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'the-events-calendar/the-events-calendar' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '5058933c3df989dedb54919bd24288cf49e53336',
+            'reference' => '9ffb4ac71fe795219c504bee0664635ff023c2e8',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
