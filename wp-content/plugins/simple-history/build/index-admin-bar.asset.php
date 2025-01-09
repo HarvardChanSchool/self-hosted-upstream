74d967e92c9f1ff7352a4ab748c1580dafd202de
Boone B Gorges <boonebgorges@gmail.com>
2025-01-09 10:21:47 -0600

    Update plugin: simple-history.
    Name: Simple History
    New version: 5.4.4
    Previous version: 5.3.0

--- wp-content/plugins/simple-history/build/index-admin-bar.asset.php
+++ wp-content/plugins/simple-history/build/index-admin-bar.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '248013133b8c97c82c43');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '3ba745b1d147660a0792');
