71a7a00529aa78f248d827bd7d07a709487f51dd
Boone B Gorges <boonebgorges@gmail.com>
2025-01-09 10:24:52 -0600

    Update plugin: gravityforms.
    Old Version: 2.8.8
    New Version: 2.9.1.2

--- wp-content/plugins/gravityforms/includes/template-library/config/class-gf-template-library-config.php
+++ wp-content/plugins/gravityforms/includes/template-library/config/class-gf-template-library-config.php
@@ -66,7 +66,7 @@ class GF_Template_Library_Config extends GF_Config {
 	}
 
 	public function should_enqueue() {
-		$current_page = trim( strtolower( rgget( 'page' ) ) );
+		$current_page = \GFForms::get_page_query_arg();
 		$gf_pages     = array( 'gf_edit_forms', 'gf_new_form' );
 
 		return in_array( $current_page, $gf_pages );
@@ -79,12 +79,13 @@ class GF_Template_Library_Config extends GF_Config {
 	 */
 	public function data() {
 		$license_info = $this->license_api->check_license();
+		$bypassTemplateLibrary = apply_filters('gform_bypass_template_library', false);
 
 		return array(
 			'components' => array(
 				'template_library' => array(
-					'endpoints' => $this->get_endpoints(),
-					'i18n'      => array(
+					'endpoints' 			=> $this->get_endpoints(),
+					'i18n'      			=> array(
 						'description'                => __( 'Form Description', 'gravityforms' ),
 						'title'                      => __( 'Form Title', 'gravityforms' ),
 						'titlePlaceholder'           => __( 'Enter the form title', 'gravityforms' ),
@@ -116,22 +117,27 @@ class GF_Template_Library_Config extends GF_Config {
 							'default' => 'This template uses Add-ons not included in your current license plan. Upgrade.',
 						),
 					),
-					'data'      => array(
+					'data'      			=> array(
 						'thumbnail_url' => \GFCommon::get_image_url( 'template-library/' ),
 						'layout'        => 'full-screen',
-						'templates'     => array_values( $this->get_templates() ),
+						'templates'     => $bypassTemplateLibrary ? array() : array_values( $this->get_templates() ),
 						'licenseType'   => $license_info->get_data_value( 'product_code' ),
 						'defaults'      => array(
-							'isLibraryOpen'             => rgget( 'page' ) === 'gf_new_form',
-							'flyoutOpen'                => false,
-							'flyoutFooterButtonLabel'   => '',
+							'isLibraryOpen'             => \GFForms::get_page_query_arg() === 'gf_new_form',
+							'flyoutOpen'                => (bool)$bypassTemplateLibrary,
+							'flyoutFooterButtonLabel'   => $bypassTemplateLibrary ? __( 'Create Form', 'gravityforms' ) : '',
 							'flyoutTitleValue'          => '',
 							'flyoutDescriptionValue'    => '',
-							'selectedTemplate'          => '',
+							'selectedTemplate'          => array(
+								'title' 	  => __( 'New Form', 'gravityforms' ),
+								'description' => __( 'A new form', 'gravityforms' ),
+								'id' 		  => 'blank',
+							),
 							'flyoutTitleErrorState'     => false,
 							'flyoutTitleErrorMessage'   => '',
 							'importError'               => false,
 							'flyoutPrimaryLoadingState' => false,
+							'bypassTemplateLibrary' => $bypassTemplateLibrary,
 						),
 					),
 				),
