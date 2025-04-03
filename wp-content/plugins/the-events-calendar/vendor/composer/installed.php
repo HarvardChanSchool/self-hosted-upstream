4363f854d21eb3521da095952f233330d7c92f0f
Boone B Gorges <boonebgorges@gmail.com>
2025-04-03 12:48:20 -0500

    Update plugin: the-events-calendar.
    Name: The Events Calendar
    New version: 6.11.0.1
    Previous version: 6.10.2

--- wp-content/plugins/the-events-calendar/vendor/composer/installed.php
+++ wp-content/plugins/the-events-calendar/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'the-events-calendar/the-events-calendar',
-        'pretty_version' => 'dev-release/T25.batman',
-        'version' => 'dev-release/T25.batman',
-        'reference' => 'e3a4a94500d555fab97420431cc7a29a298ff0a9',
+        'pretty_version' => 'dev-release/T25.daredevil.1',
+        'version' => 'dev-release/T25.daredevil.1',
+        'reference' => '6d064f3c5e9fda41c39ecef9718a37bba8265775',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'the-events-calendar/the-events-calendar' => array(
-            'pretty_version' => 'dev-release/T25.batman',
-            'version' => 'dev-release/T25.batman',
-            'reference' => 'e3a4a94500d555fab97420431cc7a29a298ff0a9',
+            'pretty_version' => 'dev-release/T25.daredevil.1',
+            'version' => 'dev-release/T25.daredevil.1',
+            'reference' => '6d064f3c5e9fda41c39ecef9718a37bba8265775',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
